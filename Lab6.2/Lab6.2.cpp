0695f7022cb7d657e3fefc297d4712e5bd02003c
Daria Chekanova <dchekanova@uni-systems.ru>
2026-04-24 13:49:42 +0300

    Lab6.2

---
+++ Lab6.2/Lab6.2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <omp.h>
+
+#pragma comment(lib, "winmm.lib")
+
+int main()
+{
+	int x = 44;
+	#pragma omp parallel for lastprivate(x)
+	for (int i = 0; i <= 10; i++)
+	{
+		printf("x  %d\n", x);
+		x = i;
+		printf("Thread number: %d x: %d\n", omp_get_thread_num(), x);
+	}
+	printf("x is %d\n", x);
+}
